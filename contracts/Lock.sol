81d5657ddb6fdb6230594de1b7fc924eb519ef03
Lance Davis <lancendavis47@gmail.com>
2022-09-10 12:37:00 -0400

    Added GET capability

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -4,14 +4,23 @@ pragma solidity ^0.8.9;
 // Import this file to use console.log
 import "hardhat/console.sol";
 // Chainlink imports
+import '@chainlink/contracts/src/v0.8/ChainlinkClient.sol';
+import '@chainlink/contracts/src/v0.8/ConfirmedOwner.sol';
 
-contract Lock {
+contract Lock is ChainlinkClient, ConfirmedOwner{
+    using Chainlink for Chainlink.Request;
+
     uint public unlockTime;
     address payable public owner;
+    // Chainlink variables
+    uint256 public score;
+    bytes32 private jobId;
+    uint256 private fee;
 
     event Withdrawal(uint amount, uint when);
+    event RequestScore(bytes32 indexed requestId, uint256 score);
 
-    constructor(uint _unlockTime) payable {
+    constructor(uint _unlockTime) ConfirmedOwner(msg.sender) payable {
         require(
             block.timestamp < _unlockTime,
             "Unlock time should be in the future"
@@ -19,6 +28,13 @@ contract Lock {
 
         unlockTime = _unlockTime;
         owner = payable(msg.sender);
+
+        setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
+        // What target oracle will we be using?
+        setChainlinkOracle();
+        // Job ID here
+        jobId = '';
+        fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
     }
 
     function withdraw() public {
@@ -38,7 +54,31 @@ contract Lock {
         * Create a Chainlink Request
         * Receive API Response
     */
-    function requestImpactScore() public returns (bytes32 ImpactScore){
+    function requestImpactScore() public returns (bytes32 requestId){
+        Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.receiveImpactScore.selector);
 
+        // Setting URL to perform GET request
+        req.add('get', 'https://impact-api.vercel.app/api/abc');
+
+        // Setting the path for the JSON response
+        req.add('path', 'body');
+
+        // add any data cleaning here
+        return sendChainlinkRequest(req, fee);
+    }
+
+    /* 
+        This function will receive the Oracle data (uint256 score). The purpose of the function is to:
+        * Receive calculated Impact Score
+        * execute an action (mint an NFT)
+    */
+    function receiveImpactScore(bytes32 _requestId, uint256 _score) public recordChainlinkFulfillment(_requestId) {
+        emit RequestScore(_requestId, _score);
+        score = _score;
+    } 
+
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+        require(link.transfer(msg.sender, link.balanceOf(address(this))), 'Unable to transfer');
     }
 }
